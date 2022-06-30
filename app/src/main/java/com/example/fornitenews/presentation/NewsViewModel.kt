8ef378fcb31a1a81c169317af383895d6e1c9c76
Andabero5 <adbeltranr@correo.udistrital.edu.co>
2022-06-29 20:40:08 -0500

    view model

--- app/src/main/java/com/example/fornitenews/presentation/NewsViewModel.kt
+++ app/src/main/java/com/example/fornitenews/presentation/NewsViewModel.kt
@@ -1,4 +1,28 @@
 package com.example.fornitenews.presentation
 
-class NewsViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
+import com.example.fornitenews.core.Resource
+import com.example.fornitenews.repository.NewsRepository
+import kotlinx.coroutines.Dispatchers
+import java.lang.Exception
+
+class NewsViewModel(private val repo : NewsRepository):ViewModel() {
+
+    fun getLatestNews() = liveData(Dispatchers.IO){
+        emit(Resource.Loading())
+        try {
+            emit(Resource.Success(repo.getLatestNews()))
+        }catch(e:Exception){
+            emit(Resource.Failure(e))
+        }
+    }
+}
+
+class NewsViewModelFactory(private val repo : NewsRepository):ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(NewsRepository::class.java).newInstance()
+    }
+
 }
